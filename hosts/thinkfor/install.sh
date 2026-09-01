bdf5b91128926352bfd74e4698d359e3a5bc6549
lxde <zacpi@pm.me>
2026-09-01 11:33:20 +0100

    updated

--- hosts/thinkfor/install.sh
+++ hosts/thinkfor/install.sh
@@ -38,6 +38,10 @@ done
 sudo dnf config-manager addrepo --overwrite \
 	--from-repofile=https://repo.librewolf.net/librewolf.repo
 
+sudo dnf install -y \
+	"https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+	"https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+
 if ((${#packages[@]})); then
 	sudo dnf install "${packages[@]}"
 fi
